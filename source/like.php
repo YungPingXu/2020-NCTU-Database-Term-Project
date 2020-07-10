d73788bc0e951c43d102b407d52d59ca9dc995f7
YungPingXu <a0976373843@gmail.com>
2020-07-10 20:10:06 +0800

    reorganized

--- source/like.php
+++ source/like.php
@@ -1,46 +1,39 @@
 <?php
-    $mysqli = new mysqli("localhost", "root", "password", "finalproject");
-    if ($mysqli->connect_errno) {
-        die("無法建立資料連接: " . $mysqli->connect_error);
-    }
-    $ip = "";
-    if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-      $ip=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else
-    {
-      $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    $good = $mysqli->query("
-        select id
-        from visitedip
-        where ip = '" . $ip . "' and id = " . $_GET['id'] . " and good = 1 and bad = 0;
-    ");
-    if($good->num_rows == 1) {
-        $mysqli->query("
-            delete from visitedip
-            where id = " . $_GET['id'] . " and ip='" . $ip . "' and good = 1 and bad = 0;
-        ");
-        $mysqli->query("
-            update animelist
-            set good = good - 1
-            where workId = " . $_GET['id'] . ";
-        ");
-        echo "cancel";
-    }else {
-        $mysqli->query("
-            insert into visitedip value('" . $ip . "', " . $_GET['id'] . ", 1, 0);
-        ");
-        $mysqli->query("
-            update animelist
-            set good = good + 1
-            where workId = " . $_GET['id'] . ";
-        ");
-        echo "success";
-    }
+$mysqli = new mysqli("localhost", "root", "password", "finalproject");
+if ($mysqli->connect_errno)
+die("無法建立資料連接: " . $mysqli->connect_error);
+$ip = "";
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+	$ip = $_SERVER['HTTP_CLIENT_IP'];
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+	$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+else
+	$ip = $_SERVER['REMOTE_ADDR'];
+$good = $mysqli->query("
+	select id
+	from visitedip
+	where ip = '" . $ip . "' and id = " . $_GET['id'] . " and good = 1 and bad = 0;
+");
+if ($good->num_rows == 1) {
+	$mysqli->query("
+		delete from visitedip
+		where id = " . $_GET['id'] . " and ip = '" . $ip . "' and good = 1 and bad = 0;
+	");
+	$mysqli->query("
+		update animelist
+		set good = good - 1
+		where workId = " . $_GET['id'] . ";
+	");
+	echo "cancel";
+} else {
+	$mysqli->query("
+		insert into visitedip value('" . $ip . "', " . $_GET['id'] . ", 1, 0);
+	");
+	$mysqli->query("
+		update animelist
+		set good = good + 1
+		where workId = " . $_GET['id'] . ";
+	");
+	echo "success";
+}
 ?>
--- source/search.php
+++ source/search.php
@@ -1,61 +1,60 @@
 <?php
 $mysqli = new mysqli("localhost", "root", "password", "finalproject");
-if ($mysqli->connect_errno) {
-    die("無法建立資料連接: " . $mysqli->connect_error);
-}
+if ($mysqli->connect_errno)
+	die("無法建立資料連接: " . $mysqli->connect_error);
 $mysqli->query("SET NAMES utf8");
 $source = $_GET['source'];
 if ($source == "Unknown") $source = "";
 $animetype = $_GET['animetype'];
 if ($animetype == "Unknown") $animetype = "";
 $result = $mysqli->query("
-    SELECT animelist.jpName, if(animelist.engName = null, '無資料', animelist.engName), animelist.workId,
-        if(animelist.animetype = 'Unknown', '無資料', animelist.animetype), if(animelist.source = 'Unknown', '無資料', animelist.source), animelist.episodes, concat(animelist.duration,' 分'), if(animelist.startyear = 0, '無資料', animelist.startyear), animelist.good, animelist.bad, animelist.workId
+	SELECT animelist.jpName, if(animelist.engName = null, '無資料', animelist.engName), animelist.workId,
+		if(animelist.animetype = 'Unknown', '無資料', animelist.animetype), if(animelist.source = 'Unknown', '無資料', animelist.source), animelist.episodes, concat(animelist.duration,' 分'), if(animelist.startyear = 0, '無資料', animelist.startyear), animelist.good, animelist.bad, animelist.workId
 	FROM animelist
-    where (animelist.jpName like '%" . $_GET['search'] . "%'
-          or animelist.engName like '%" . $_GET['search'] . "%')
-          and animelist.animetype like '%" . $animetype . "%' and
-        animelist.source like '%" . $source . "%' and
-        animelist.episodes >= " . $_GET['episodes'] . " and 
-        animelist.duration >= " . $_GET['duration'] . " and
-        animelist.startyear >= " . $_GET['startyear'] . "
-    order by animelist.good desc
-    LIMIT " . $_GET['num'] . ";
+	where (animelist.jpName like '%" . $_GET['search'] . "%'
+			or animelist.engName like '%" . $_GET['search'] . "%')
+			and animelist.animetype like '%" . $animetype . "%' and
+		animelist.source like '%" . $source . "%' and
+		animelist.episodes >= " . $_GET['episodes'] . " and 
+		animelist.duration >= " . $_GET['duration'] . " and
+		animelist.startyear >= " . $_GET['startyear'] . "
+	order by animelist.good desc
+	LIMIT " . $_GET['num'] . ";
 ");
-if ($result->num_rows == 0) {
-    echo "<span style='text-align: center;'>查無結果</span>";
-} else {
-    echo "<table align='center' border='1'><tr>";
-    echo "<tr>
-            <th style='width: 400px;'>日文名稱</th>
-            <th style='width: 400px;'>英文名稱</th>
-            <th>種類</th>
-            <th>原作</th>
-            <th>集數</th>
-            <th>時間</th>
-            <th>年份</th>
-            <th>按讚</th>
-        </tr>";
-    $cnt = 0;
-    while ($row = $result->fetch_row()) {
-        echo "<tr>";
-        echo "<td>" . $row[0] . "</td>";
-        echo "<td>" . $row[1] . "</td>";
-        echo "<td>" . $row[3] . "</td>";
-        echo "<td>" . $row[4] . "</td>";
-        echo "<td>" . $row[5] . "</td>";
-        echo "<td>" . $row[6] . "</td>";
-        echo "<td>" . $row[7] . "</td>";
-        echo "<input id='recommend-id" . $cnt . "' type='hidden' value='" . $row[2] . "'>";
-        echo "<td><input id='good" . $row[10] . "' class='img' type='image' img='' src='/img/like.PNG' onclick='like(" . $row[10] . ")'><span id='like" . $row[10] . "'>" . $row[8] . "</span><input id='bad" . $row[10] . "' class='img' type='image' img='' src='/img/dislike.PNG' onclick='unlike(" . $row[10] . ")'><span id='unlike" . $row[10] . "'>" . $row[9] . "</span></td>";
-        echo "<td>
-            <button class='recommend-button' id='recommend-button" . $cnt . "' onclick='recommend(" . $cnt++ . ")'>
-            <!--<i class='fa fa-circle-o-notch fa-spin'></i>-->開始推薦
-            </button>
-        </td>";
-        echo "</tr>";
-    }
-    echo "</table>";
+if ($result->num_rows == 0)
+	echo "<span style='text-align: center;'>查無結果</span>";
+else {
+	echo "<table align='center' border='1'><tr>";
+	echo "<tr>
+			<th style='width: 400px;'>日文名稱</th>
+			<th style='width: 400px;'>英文名稱</th>
+			<th>種類</th>
+			<th>原作</th>
+			<th>集數</th>
+			<th>時間</th>
+			<th>年份</th>
+			<th>按讚</th>
+		</tr>";
+	$cnt = 0;
+	while ($row = $result->fetch_row()) {
+		echo "<tr>";
+		echo "<td>" . $row[0] . "</td>";
+		echo "<td>" . $row[1] . "</td>";
+		echo "<td>" . $row[3] . "</td>";
+		echo "<td>" . $row[4] . "</td>";
+		echo "<td>" . $row[5] . "</td>";
+		echo "<td>" . $row[6] . "</td>";
+		echo "<td>" . $row[7] . "</td>";
+		echo "<input id='recommend-id" . $cnt . "' type='hidden' value='" . $row[2] . "'>";
+		echo "<td><input id='good" . $row[10] . "' class='img' type='image' img='' src='/img/like.PNG' onclick='like(" . $row[10] . ")'><span id='like" . $row[10] . "'>" . $row[8] . "</span><input id='bad" . $row[10] . "' class='img' type='image' img='' src='/img/dislike.PNG' onclick='unlike(" . $row[10] . ")'><span id='unlike" . $row[10] . "'>" . $row[9] . "</span></td>";
+		echo "<td>
+			<button class='recommend-button' id='recommend-button" . $cnt . "' onclick='recommend(" . $cnt++ . ")'>
+			<!--<i class='fa fa-circle-o-notch fa-spin'></i>-->開始推薦
+			</button>
+		</td>";
+		echo "</tr>";
+	}
+	echo "</table>";
 }
 $result->free();
 $mysqli->close();
